4759fca6119bea7c1396730b0eab596cb59ef9b0
KMT-KJW <gjkjw_030@naver.com>
2025-12-15 15:48:42 +0900

    gazebo harmonic 으로 변경 시도

--- launch/gimbal_gazebo.launch.py
+++ launch/gimbal_gazebo.launch.py
@@ -6,43 +6,34 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import TimerAction
 
-
-
 def generate_launch_description():
     pkg_share = FindPackageShare('gimbal_demo')
     xacro_path = PathJoinSubstitution([pkg_share, 'urdf', 'gimbal.xacro'])
-    rviz_config = PathJoinSubstitution([pkg_share, 'rviz', 'camera.rviz'])
 
     robot_description = Command(['xacro ', xacro_path])
 
+    # 1. 로봇 모델 퍼블리셔
     robot_state_publisher = Node(
-        package= 'robot_state_publisher',
-        executable= 'robot_state_publisher',
-        name= 'robot_state_publisher',
-        output= 'screen',
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
         parameters=[{'robot_description': robot_description}],
-    )
-
-    rviz_node = Node(
-        package= 'rviz2',
-        executable= 'rviz2',
-        name= 'rviz2',
-        output= 'screen',
-        arguments=['-d', rviz_config]
     )
 
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [FindPackageShare('gazebo_ros'), '/launch/gazebo.launch.py']
-        )
+            [FindPackageShare('ros_gz_sim'), '/launch/gz_sim.launch.py']
+        ),
+        launch_arguments={'gz_args': '-s -r -v 4 empty.sdf'}.items(),
     )
 
     spawn_entity = Node(
-        package= 'gazebo_ros',
-        executable= 'spawn_entity.py',
-        arguments= ['-topic', 'robot_description',
-                    '-entity', 'gimbal'],
-        output= 'screen',
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description',
+                   '-name', 'gimbal'],
+        output='screen',
     )
 
     joint_state_spawner = Node(
@@ -62,8 +53,8 @@ def generate_launch_description():
     )
 
     joint_state_spawner_delayed = TimerAction(
-    period=3.0,
-    actions=[joint_state_spawner]
+        period=3.0,
+        actions=[joint_state_spawner]
     )
 
     gimbal_spawner_delayed = TimerAction(
@@ -71,11 +62,30 @@ def generate_launch_description():
         actions=[gimbal_spawner]
     )
 
+    gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            '/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+        ],
+        output='screen'
+    )
+
+    foxglove_bridge = Node(
+        package='foxglove_bridge',
+        executable='foxglove_bridge',
+        name='foxglove_bridge',
+        output='screen',
+    )
+
     return LaunchDescription([
-        rviz_node,
         gazebo_launch,
         robot_state_publisher,
         spawn_entity,
         joint_state_spawner_delayed,
         gimbal_spawner_delayed,
+        gz_bridge,
+        foxglove_bridge, #
     ])
